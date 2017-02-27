4ba3be184f46a66ba772140eb0087fe2f63cf16d
DrewWarrenTIY <drewwarrentiy@gmail.com>
2017-02-27 10:39:47 -0700

    Include JQuery

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('cheese');
+var domainList = ['drew.daplie.me', 'test.daplie.me']
+
+console.log(domainList);
+
+// When '.' is pressed, show a domain from list.
+$('body').on('click', '.datInput', function (ev) {
+  console.log('cheese');
+})
